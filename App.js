21a71e36642a6cd2b25468f623f2de978f8bed3a
Cheobaby <noelmtzramirez1.6@gmail.com>
2024-03-09 09:51:36 -0600

    segundo cambio

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hla mundo</Text>
+      <button title='CLICK'></button>
       <StatusBar style="auto" />
     </View>
   );
